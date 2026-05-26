81d2a2bc8125a53bc437f03feb5e613f7d6684e1
Contrucc <contrucc300@gmail.com>
2026-05-26 12:25:37 -0300

    Delete js directory

--- js/supabase.js
+++
@@ -1,9 +0,0 @@
-// Configuración de Supabase
-const supabaseUrl = 'https://zjnourjlpssxjbrtndxu.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpqbm91cmpscHNzeGpicnRuZHh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk3ODU4MDMsImV4cCI6MjA5NTM2MTgwM30.153hHzbc95F3L1mczZCP1roLyW_CF1t08YjiKZTOSwk';
-
-// Crear cliente de Supabase
-const supabase = window.supabase.createClient(supabaseUrl, supabaseAnonKey);
-
-// Variables globales
-window.supabaseClient = supabase;
